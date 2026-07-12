dbbda3dd66f03db72dc0fdffe9b6f7e214785520
Bufka2011 <bufka.bulanov@gmail.com>
2026-07-12 13:34:40 -0600

    Add null guards to registerAtmosphere, unregisterAtmosphere, and addGrowth

--- src/main/java/com/hbm/handler/atmosphere/ChunkAtmosphereHandler.java
+++ src/main/java/com/hbm/handler/atmosphere/ChunkAtmosphereHandler.java
@@ -242,6 +242,7 @@ public class ChunkAtmosphereHandler {
 	 */
 	public void registerAtmosphere(IAtmosphereProvider handler) {
 		HashMap<IAtmosphereProvider, AtmosphereBlob> blobs = worldBlobs.get(handler.getWorld().provider.dimensionId);
+		if(blobs == null) return;
 		AtmosphereBlob blob = blobs.get(handler);
 
 		if(blob == null) {
@@ -253,7 +254,7 @@ public class ChunkAtmosphereHandler {
 
 	public void unregisterAtmosphere(IAtmosphereProvider handler) {
 		HashMap<IAtmosphereProvider, AtmosphereBlob> blobs = worldBlobs.get(handler.getWorld().provider.dimensionId);
-		blobs.remove(handler);
+		if(blobs != null) blobs.remove(handler);
 	}
 
 
@@ -464,11 +465,13 @@ public class ChunkAtmosphereHandler {
 
 	public void addGrowth(World world, Block from, Block into, int x, int y, int z, int size) {
 		Queue<Growth> growths = growthMap.get(world.provider.dimensionId);
+		if(growths == null) return;
 		growths.add(new Growth(from, into, x, y, z, size));
 	}
 
 	public void addGrowth(World world, Block from, Block into, int x, int y, int z, int count, int size) {
 		Queue<Growth> growths = growthMap.get(world.provider.dimensionId);
+		if(growths == null) return;
 		for(int i = 0; i < count; i++) {
 			growths.add(new Growth(from, into, x, y, z, size));
 		}
