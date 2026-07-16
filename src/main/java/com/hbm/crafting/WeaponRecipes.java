42753af4a9ae625b090d99768fc9ae81caf9cd79
Bufka2011 <bufka.bulanov@gmail.com>
2026-07-16 11:33:13 -0600

    Convert weapon table recipe to gerg, requires WRENCH

--- src/main/java/com/hbm/crafting/WeaponRecipes.java
+++ src/main/java/com/hbm/crafting/WeaponRecipes.java
@@ -41,7 +41,7 @@ public class WeaponRecipes {
 	public static void register() {
 
 		//Weapon mod table
-		CraftingManager.addRecipeAuto(new ItemStack(ModBlocks.machine_weapon_table, 1), new Object[] { "P P", "TCT", "TST", 'P', GUNMETAL.plate(), 'T', STEEL.ingot(), 'C', Blocks.crafting_table, 'S', STEEL.block() }, GergToolType.WRENCH);
+		GergRecipeHelper.addGergShaped(new ItemStack(ModBlocks.machine_weapon_table, 1), new Object[] { "P P", "TCT", "TST", 'P', GUNMETAL.plate(), 'T', STEEL.ingot(), 'C', Blocks.crafting_table, 'S', STEEL.block() }, GergToolType.WRENCH);
 
 		//SEDNA Parts
 		CraftingManager.addRecipeAuto(new ItemStack(ModItems.part_stock, 1, Mats.MAT_WOOD.id), new Object[] { "WWW", "  W", 'W', KEY_PLANKS });
